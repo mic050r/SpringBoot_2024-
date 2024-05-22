145c8a90b8a0dc66f5bb14b14b2cd818cdb88cd3
mic050r <s2216@e-mirim.hs.kr>
2024-05-22 09:04:04 +0900

    @RequestMapping 으로 공통된 부분 작성하기

--- UrlMapping2/src/main/java/kr/study/UrlMapping2/controller/TestController.java
+++ UrlMapping2/src/main/java/kr/study/UrlMapping2/controller/TestController.java
@@ -2,21 +2,25 @@ package kr.study.UrlMapping2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/sub")
 public class TestController {
 
-    @GetMapping("/test1")
+    @GetMapping("test1")
+    @ResponseBody // 굳이 화면을 만들지 않아도 텍스트로 화면에 출력
     public String test1() {
         return "test1";
     }
 
-    @GetMapping("sub/test2")
+    @GetMapping("test2")
     public String test2() {
         return "test2";
     }
 
-    @GetMapping("sub/test3")
+    @GetMapping("test3")
     public String test3() {
         return "test3";
     }

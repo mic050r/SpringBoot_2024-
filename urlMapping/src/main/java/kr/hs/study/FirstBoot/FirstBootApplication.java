0364513860a6d508bf1795afe4bb0dfaaa8d81fb
mic050r <s2216@e-mirim.hs.kr>
2024-08-14 09:15:57 +0900

    Unversioned Files

---
+++ urlMapping/src/main/java/kr/hs/study/FirstBoot/FirstBootApplication.java
@@ -0,0 +1,12 @@
+package kr.hs.study.FirstBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class FirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBootApplication.class, args);
+	}
+
+}

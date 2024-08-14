3b37041d21199321daf8886210df1f50f9375f7e
mic050r <s2216@e-mirim.hs.kr>
2024-08-14 10:01:57 +0900

    Feat : 구구단 get, post 컨트롤러 생성

---
+++ SpringPrj/src/main/java/kr/hs/study/SpringPrj/controller/TestController.java
@@ -0,0 +1,27 @@
+package kr.hs.study.SpringPrj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/table")
+    public String table() {
+        return "table";
+    }
+
+    @PostMapping("/table")
+    public String table(@RequestParam("num") int num, Model model) {
+        StringBuilder result = new StringBuilder();
+        for(int i = 1; i <= 9; i++) {
+            String val = num + "X" + i + "=" + num*i + "\r\n";
+            result.append(val);
+        }
+        model.addAttribute("result", result.toString());
+        return "table";
+    }
+}
